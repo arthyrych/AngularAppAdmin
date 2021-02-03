2b7f0c3d7636724a8cb3d1964dde0c624475bcd6
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-02-03 17:00:25 +0200

    created

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,55 @@
+/// <reference types="cypress" />
+
+describe("first test describe", () => {
+
+    beforeEach("code for every test", () => {
+
+    })
+
+    it("first test it", () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // by Tag Name
+        cy.get('input')
+
+        // by ID
+        cy.get('#inputEmail1')
+
+        // by Class name
+        cy.get('.input-full-width')
+
+        // by Attribute name
+        cy.get('[placeholder]')
+
+        // by Attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        // by Class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        // by Tag name and Attribute with value
+        cy.get('input[placeholder="Email"]')
+
+        // by two different attributes
+        cy.get('[placeholder="Email"][fullwidth]')
+
+        // by ta name, attribute with value, ID and class name
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        // the most recommended way by Cypress
+        cy.get('[data-cy="imputEmail1"]')
+
+    })
+
+    it("second test it", () => {
+
+    })
+
+    it("third test it", () => {
+
+    })
+
+})
