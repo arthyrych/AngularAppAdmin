a0d64c7baa116a92a774d2b9fdbff1c8f332910a
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-02-05 18:47:40 +0200

    test added

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+
+describe('test description', () => {
+
+    it('first test', () => {
+
+        cy.visit('/')
+
+        cy.contains('Layout')
+            .click()
+        
+        cy.contains('Stepper')
+            .click()
+
+        cy.get('[class="col-md-12 col-lg-12 col-xxxl-12"]')
+
+    })
+
+})
