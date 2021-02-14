2817178a4f01025a4c603df019c750870bba30a3
Artem <arthyrych@gmail.com>
2021-02-14 19:51:22 +0200

    updated

---
+++ cypress/integration/listsAndDropdowns.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+
+describe('lists and drowdowns test suite', ()=> {
+
+    beforeEach(()=> {
+
+        cy.visit('/')
+
+    })
+
+
+    it('some', ()=> {
+
+        cy.get('nav nb-select').click()
+        cy.get('.options-list').contains('Dark').click()
+        cy.get('nav nb-select').should('contain', 'Dark')
+        cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+    })
+
+})
