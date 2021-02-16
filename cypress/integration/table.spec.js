15e4cff92d88fbf4b9c64a83aa02c9d44ea85ff2
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-02-16 20:02:24 +0200

    updated

--- cypress/integration/table.spec.js
+++ cypress/integration/table.spec.js
@@ -8,7 +8,15 @@ describe ('table test suite', ()=> {
 
     it('table test', ()=> {
 
-        
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        cy.get('tbody').contains('tr', 'Larry').then( tableRow => {
+            cy.wrap(tableRow).find('.nb-edit').click()
+            cy.wrap(tableRow).find('[placeholder="Age"]').type('25')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+       })
 
     })
 
