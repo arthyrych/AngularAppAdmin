c3470faffc91d4ac55035cf946a838e23d0a020a
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-02-16 18:44:51 +0200

    updated

---
+++ cypress/integration/table.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe ('table test suite', ()=> {
+
+    beforeEach(()=> {
+        cy.visit('/')
+    })
+
+    it('table test', ()=> {
+
+        
+
+    })
+
+})
