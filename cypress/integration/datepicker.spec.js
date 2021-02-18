37f0f068183f0a0340ec6cdd4df68ad7e69e4b81
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-02-18 10:34:56 +0200

    datepicker test created

---
+++ cypress/integration/datepicker.spec.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+
+describe('datepicker test suite', ()=> {
+
+    beforeEach(()=> {
+        cy.visit('/')
+    })
+
+    it('datepicker test', ()=> {
+
+        function selectDayFromCurrent(day){
+
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonth = date.toLocaleString('default', {month: 'short'})
+            let dateAssert = futureMonth + ' ' + futureDay + ', ' + date.getFullYear()
+
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then( dateAttribute => {
+                if(!dateAttribute.includes(futureMonth)){
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDayFromCurrent(day)
+                } else {
+                    cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+                }
+            })
+            return dateAssert
+        }
+
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+        
+        cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
+            cy.wrap(input).click()
+            let dateAssert = selectDayFromCurrent(1)
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
+        })
+        
+    })
+})
