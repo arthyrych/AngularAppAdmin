d700c087223734ee979ce03821f244022b5003c3
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-02-19 10:02:06 +0200

    updated

--- cypress/integration/9-popup-tooltip.spec.js
+++ cypress/integration/9-popup-tooltip.spec.js
@@ -19,7 +19,23 @@ describe ('popup, tooltip, dialog box test suite', ()=> {
 
         cy.contains('Tables & Data').click()
         cy.contains('Smart Table').click()
+
+        // 1 example
         cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', (confirm)=> {
+            expect(confirm).to.equal('Are you sure you want to delete?')
+        }) 
+
+        // 2 example (recommended)
+        const stub = cy.stub()
+        cy.on('window:confirm', stub)
+        cy.get('tbody tr').first().find('.nb-trash').click().then(()=> {
+            expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+        })
+
+        // 3 example (to cancel)
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', () => false)
     })
 
 })
