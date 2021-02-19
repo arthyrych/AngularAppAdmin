8c6af1ff4aba586a9f4f5cb3155a77a2aea8a1f4
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-02-19 18:34:19 +0200

    assertions added

---
+++ cypress/integration/10-assertions.spec.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+describe('assertions test suite', ()=> {
+
+    beforeEach(()=> {
+        cy.visit('/')
+    })
+
+    it('assertions', ()=> {
+
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        // should format
+        cy.get('[for="exampleInputEmail1"]')
+            .should('contain', 'Email address')
+            .should('have.class', 'label')
+            .and('have.text', 'Email address')
+
+        // expect format
+        cy.get('[for="exampleInputEmail1"]').then(label => {
+            expect(label.text()).to.equal('Email address')
+            expect(label).to.have.class('label')
+            expect(label).to.have.text('Email address')
+        })
+
+
+    })
+})
