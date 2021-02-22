77bd86c5eafdc17446e1b25f86547f78c008bc65
arthyrych <arthyrych@arthyrych.SOT.COM>
2021-02-22 19:02:27 +0200

    added e2e test with page objects

--- cypress/integration/11-page-object.js
+++ cypress/integration/11-page-object.js
@@ -1,6 +1,9 @@
 /// <reference types="cypress" />
 
+import { onDatepickerPage } from "../support/pageObjects/datepickerPage"
+import { onFormLayoutsPage } from "../support/pageObjects/formLayoutsPage"
 import { navigateTo } from "../support/pageObjects/navigationPage"
+import { onSmartTablePage } from "../support/pageObjects/smartTablePage"
 
 describe('page object test suite', ()=> {
 
@@ -9,12 +12,24 @@ describe('page object test suite', ()=> {
     })
 
     it('verify navigations across the pages', ()=> {
-
         navigateTo.formLayoutsPage()
         navigateTo.datepickerPage()
         navigateTo.toasterPage()
         navigateTo.smartTablePage()
         navigateTo.tooltipPage()
+    })
+
+    it.only('should submit Inline and Basic form and select tomorrow date in the calendar', ()=> {
+        navigateTo.formLayoutsPage()
+        onFormLayoutsPage.submitInlineFormWithNameAndEmail('Artem', 'test@test.com')
+        onFormLayoutsPage.submitBasicFormWithEmailAndPass('test@test.com', 'fakepass')
+        navigateTo.datepickerPage()
+        onDatepickerPage.selectCommonDatepickerDateFromToday(1)
+        onDatepickerPage.selectDatepickerWithRangeFromToday(7, 14)
+        navigateTo.smartTablePage()
+        onSmartTablePage.addNewRecordWithFirstAndLastName('Artem', 'Hyrych')
+        onSmartTablePage.updateAgeByFirstName('Artem', '35')
+        onSmartTablePage.deleteRowByIndex(1)
     })
 
 })
---
+++ cypress/support/pageObjects/formLayoutsPage.js
@@ -0,0 +1,22 @@
+export class FormLayoutsPage {
+
+    submitInlineFormWithNameAndEmail(name, email){
+        cy.contains('nb-card', 'Inline form').find('form').then( form => {
+            cy.wrap(form).find('[placeholder="Jane Doe"]').type(name)
+            cy.wrap(form).find('[placeholder="Email"]').type(email)
+            cy.wrap(form).find('[type="checkbox"]').check({force: true})
+            cy.wrap(form).submit()
+        })
+    }
+
+    submitBasicFormWithEmailAndPass(email, pass){
+        cy.contains('nb-card', 'Basic form').find('form').then( form => {
+            cy.wrap(form).find('[placeholder="Email"]').type(email)
+            cy.wrap(form).find('[placeholder="Password"]').type(pass)
+            cy.wrap(form).find('[type="checkbox"]').check({force: true})
+            cy.wrap(form).submit()
+        })
+    }
+}
+
+export const onFormLayoutsPage = new FormLayoutsPage()
